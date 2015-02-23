e3f464fb01b4f886f6f18069adde6279d89c9483
Josef Šimánek <josef.simanek@gmail.com>
2015-02-23 20:53:03 +0100

    Test object_id is not changed during meltdown.

--- test/test_melt.rb
+++ test/test_melt.rb
@@ -2,9 +2,7 @@ require 'minitest_helper'
 require 'melt'
 
 class TestMelt < Minitest::Test
-  def test_it_does_something_useful
-    icicle = "----->"
-
+  def test_it_melt
     # winter
     icicle.freeze
     assert icicle.frozen?, 'Icicle is not frozen!'
@@ -12,5 +10,21 @@ class TestMelt < Minitest::Test
     # spring
     icicle.melt
     refute icicle.frozen?, 'Icile is not melted!'
+  end
+
+  def test_melted_is_same_object
+    oid = icicle.object_id
+
+    # winter
+    icicle.freeze
+    assert_equal oid, icicle.object_id
+
+    # spring
+    icicle.melt
+    assert_equal oid, icicle.object_id
+  end
+
+  def icicle
+    @icicle ||= "----->"
   end
 end
